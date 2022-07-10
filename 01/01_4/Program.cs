7590478848804182413ea356de4e67aa247d67c3
Dmitri Buchnev <skyfaller@mail.ru>
2022-07-10 12:48:49 +0300

    04

---
+++ 01/01_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача №7
+
+Console.WriteLine("Ввод числа");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
