3b6844742c92e5eb393cd48b1f8bf8db8279bc7f
Dmitri Buchnev <skyfaller@mail.ru>
2022-07-10 11:05:19 +0300

    03

--- 01/01_3/Program.cs
+++ 01/01_3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача №3
 
+Console.WriteLine("write a num a");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+Console.WriteLine ("понедельник");
+
+else if (a == 2)
+Console.WriteLine ("вторник");
+
+else if (a == 3)
+Console.WriteLine ("среда");
